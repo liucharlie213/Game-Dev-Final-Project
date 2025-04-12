c230d146fa152fb5d85284df4c31b417b7a46af1
knb2139 <knb2139@columbia.edu>
2025-04-12 15:39:56 -0400

    endless runner scene prototype

---
+++ party_prototype/scripts/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+const GRAVITY = 7200.0
+const JUMP_SPEED = -1800.0
+
+
+func _physics_process(delta: float) -> void:
+	velocity.y += GRAVITY * delta
+	if is_on_floor():
+		if Input.is_action_pressed("ui_accept"):
+			velocity.y = JUMP_SPEED
+		elif Input.is_action_pressed("ui_down"):
+			%AnimatedSprite2D.play("duck")
+		else:
+			%AnimatedSprite2D.play("run")
+	else:
+		%AnimatedSprite2D.play("jump")
+			
+
+	move_and_slide()
