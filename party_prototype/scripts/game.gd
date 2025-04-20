c9252bc447aa574d3dfad94b986e074432d7e865
knb2139 <knb2139@columbia.edu>
2025-04-20 19:13:56 -0400

    major updates to infinite runner scene

--- party_prototype/scripts/game.gd
+++ party_prototype/scripts/game.gd
@@ -2,10 +2,19 @@ extends Node2D
 
 var box_scene = preload("res://scenes/box.tscn")
 var garbage_scene = preload("res://scenes/garbage.tscn")
+var streelight_scene = preload("res://scenes/street_light.tscn")
+var bench_scene = preload("res://scenes/bench.tscn")
+var wiz_scene = preload("res://scenes/wiz.tscn")
+var const_scene = preload("res://scenes/const.tscn")
+var worker_scene = preload("res://scenes/worker.tscn")
+var stopsign_scene = preload("res://scenes/stopsign.tscn")
+var hydrant_scene = preload("res://scenes/hydrant.tscn")
 var obstacle_types = [box_scene, garbage_scene]
+var s_d_types = [wiz_scene, const_scene, worker_scene, stopsign_scene, hydrant_scene]
 var obstacles : Array 
+var street_decor : Array
 
-const START_POS := Vector2i(0, 0)
+const START_POS := Vector2i(100, 0)
 const MOUSE_START_POS := Vector2i(-300, 0)
 const CAM_START_POS := Vector2i(650, -350)
 
@@ -15,6 +24,10 @@ const MAX_SPEED : int = 25
 var screen_size : Vector2i
 var ground_height : int
 var last_obs
+var last_s_d
+var obstacles_hit = 0
+var street_light
+var bench
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -29,12 +42,24 @@ func new_game():
 	%Player.velocity= Vector2i(0, 0)
 	$Camera2D.position = CAM_START_POS
 	$road.position = Vector2i(0,0)
+	street_light = streelight_scene.instantiate()
+	bench = bench_scene.instantiate()
+	
+	street_light.position = Vector2i(-400,150)
+	bench.position = Vector2i(-550,200)
+	
+	add_child(street_light)
+	add_child(bench)
+	
+	$GameOverFail.hide()
+	$GameOverSuccess.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	speed = START_SPEED
 	
 	generate_obs()
+	generate_s_d()
 	
 	%Player.position.x += speed
 	$Camera2D.position.x += speed
@@ -43,7 +68,12 @@ func _process(delta: float) -> void:
 	if $Camera2D.position.x - $road.position.x > screen_size.x * 0.8:
 		$road.position.x += screen_size.x
 		
-	pass
+	if $Camera2D.position.x - street_light.position.x > screen_size.x * 1:
+		street_light.position.x += screen_size.x * 2
+		
+	if $Camera2D.position.x - bench.position.x > screen_size.x * 1:
+		bench.position.x += screen_size.x * 2
+	#pass
 
 func generate_obs():
 	if obstacles.is_empty() or last_obs.position.x < $Camera2D.position.x + randi_range(20, 200):
@@ -59,5 +89,43 @@ func generate_obs():
 
 func add_obs(obs, x, y):
 	obs.position = Vector2i(x, y)
+	obs.body_entered.connect(hit_obs.bind(obs))
 	add_child(obs)
 	obstacles.append(obs)
+	
+func generate_s_d():
+	if street_decor.is_empty() or last_s_d.position.x < $Camera2D.position.x + randi_range(20, 200):
+		var s_d_type = s_d_types[randi() % s_d_types.size()]
+		var s_d
+		s_d = s_d_type.instantiate()
+		var s_d_height = s_d.get_node("Sprite2D").texture.get_height()
+		var s_d_scale = s_d.get_node("Sprite2D").scale
+		var s_d_x = $Camera2D.position.x + screen_size.x + randi_range(100, 400)
+		var s_d_y = screen_size.y - ground_height - (s_d_height * s_d_scale.y /2 ) + 5
+		last_s_d = s_d 
+		add_s_d(s_d, s_d_x, randi_range(80, 250))
+
+func add_s_d(obs, x, y):
+	obs.position = Vector2i(x, y)
+	#obs.body_entered.connect(hit_obs.bind(obs))
+	add_child(obs)
+	street_decor.append(obs)
+
+func hit_obs(body, obs):
+	if body.name == "Player" && obs.visible:
+		obstacles_hit+=1
+		if obstacles_hit == 1:
+			%Player.position.x -= 200
+			#make obstacle disseapear
+			obs.visible = false;
+		else:
+			game_over()
+		
+
+func game_over():
+	get_tree().paused = true
+	$GameOverFail.show()
+
+
+func _on_timer_bar_2_made_it_home() -> void:
+	$GameOverSuccess.show()
