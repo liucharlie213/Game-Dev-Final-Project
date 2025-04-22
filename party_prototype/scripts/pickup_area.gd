356f30e7316ead31a0dea10ba61c4ec0c7af087e
Shanshan WU <ssincn@outlook.com>
2025-04-22 06:18:37 -0400

    1. add alchohol scene 2. add pickupzone scene. You can add it as the child of a node to make    it pickable 3. add feature: press E to pick up 4. there will be a pick prompt shown when the player is able to pick up an    item. 5. Add the door in the first scene to connect two scenes 6. I'm not sure if the endlessrunner part works correctly after merge.    Please check.

---
+++ party_prototype/scripts/pickup_area.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+@onready var prompt: Label = $PickLabel
+@onready var prompt_background: Sprite2D = $PickBackground
+
+var pickedup: bool = false #check if itself is picked up
+
+func _ready() -> void:
+	set_label_visible(false)
+
+func _on_body_entered(body: Node2D) -> void:
+	print("pick")
+	body.can_pickup_item = self
+	set_label_visible(true)
+	
+
+func _on_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	set_label_visible(false)
+	
+
+func _process(delta: float) -> void:
+	if pickedup:
+		get_parent().queue_free()
+		
+		
+func set_label_visible(status: bool):
+	prompt.visible = status
+	prompt_background.visible = status
